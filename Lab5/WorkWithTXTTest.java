d048f5aacbd09cd0a9437a5a663364595db51c8d
Nairobi2302 <137928950+Nairobi2302@users.noreply.github.com>
2024-01-11 01:39:52 +0300

    Create WorkWithTXTTest.java

---
+++ Lab5/WorkWithTXTTest.java
@@ -0,0 +1,13 @@
+package lab5;
+import static org.junit.jupiter.api.Assertions.*;
+
+class WorkWithTXTTest {
+
+    @org.junit.jupiter.api.Test
+    void readFromFileTXT() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void writeInFileTXT() {
+    }
+}
