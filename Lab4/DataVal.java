24bca5c988b41b3677c51a6cbd9f27a558b4cb4c
Nairobi2302 <137928950+Nairobi2302@users.noreply.github.com>
2024-01-11 01:09:38 +0300

    Create DataVal.java

---
+++ Lab4/DataVal.java
@@ -0,0 +1,23 @@
+package lab4;
+import java.util.ArrayList;
+
+public class DataValidator {
+
+    public static void validateData(ArrayList<Fabric> fabrics) {
+        System.out.println("--- Data Validation ---");
+        for (Fabric fabric : fabrics) {
+            if (fabric.getBrand() == null || fabric.getType() == null || fabric.getWeight() == null) {
+                System.out.println("Invalid data found:");
+                System.out.println("Brand: " + fabric.getBrand());
+                System.out.println("Type: " + fabric.getType());
+                System.out.println("Weight: " + fabric.getWeight());
+            } else {
+                System.out.println("Data is valid for:");
+                System.out.println("Brand: " + fabric.getBrand());
+                System.out.println("Type: " + fabric.getType());
+                System.out.println("Weight: " + fabric.getWeight());
+            }
+            System.out.println("----------------------");
+        }
+    }
+}
